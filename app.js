46287f243ef5240301572873525189e5f3773a71
clericlvl2 <budarnykh.a@yandex.ru>
2022-10-29 02:14:56 +0700

    add basic chat layout and functionality

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(port, () => {
+    console.log('listening port 3000')
+});
